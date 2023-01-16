8d6f34664a2c34b4dff988732ba97d057ee18c3a
steffenrog <steffen@rognet.net>
2023-01-16 22:18:19 +0100

    yes

--- pi_pico/analog_reading.py
+++ pi_pico/analog_reading.py
@@ -10,18 +10,18 @@ xaxi = analogio.AnalogIn(board.GP27_A1)
 
 readings = 0
 
-# while True:
-#         x = xaxi.value
-#         y = yaxi.value
-#         elapsed_time = time.monotonic() - start_time
-#         elapsed_time_format = "{:.2f}".format(elapsed_time)
-#         readings += 1
-#         readingsWanted = 1000
-#         if(readings == readingsWanted):
-#             print("{} sek - x: {}, y: {}, Readings done: {}".format(elapsed_time_format, x, y,readings))
-
-
 while True:
-    x = xaxi
-    y = yaxi
-    print (x,y)
+        x = xaxi.value
+        y = yaxi.value
+        elapsed_time = time.monotonic() - start_time
+        elapsed_time_format = "{:.2f}".format(elapsed_time)
+        readings += 1
+        readingsWanted = 1000
+        if(readings == readingsWanted):
+            print("{} sek - x: {}, y: {}, Readings done: {}".format(elapsed_time_format, x, y,readings))
+
+
+# while True:
+#     x = xaxi.value
+#     y = yaxi.value
+#     print (x,y)
--- pi_pico/pico_joystick.py
+++ pi_pico/pico_joystick.py
@@ -31,40 +31,58 @@ spi = busio.SPI(board.GP18, board.GP19, board.GP16)
 yaxi = analogio.AnalogIn(board.GP26_A0)
 xaxi = analogio.AnalogIn(board.GP27_A1)
 can_bus = CAN(spi, cs)
+joy_res = 12
 
-##To use filters, 2 filters can be used
+##To use can filters, 2 filters can be used
 class Match:
     def __init__(self,address,mask,extended: bool):
         self.address = address
         self.mask = mask
         self.extended = extended
 
-
 ##Send joystick values, adapted to receiving software.
 async def send_joystick_position(x, y):
-    joystick_res = 12
-    x= (x/65536.0)*(2**joystick_res)
-    y= (y/65536.0)*(2**joystick_res)
-
     id = 0x18fdd6F1 ##Modify ID
+    
+    #x = ((x/65536.0) * 2048) - 1024 ##Tested for 1024
+    #y = ((y/65536.0) * 2048) - 1024 ##Tested for 1024
+    #x = ((x/65536.0) * 8192) - 4096 ##Tested for 4096
+    #y = ((y/65536.0) * 8192) - 4096 ##Tested for 4096
+    
+    x = ((x/2**16)*(2**(joy_res+1)))-(2**joy_res) ##Works pretty good 12bit 4096
+    y = ((y/2**16)*(2**(joy_res+1)))-(2**joy_res) ##Works pretty good 12bit 4096
 
     data = [0x01, 0x00, 0x01, 0x00, 0xff, 0x00, 0x00, 0x1f]
-    data[0] = 0x03
-    data[2] = 0x03
-    if x < 0: data[0] = 0x10    ##If we are using negative values
-    if y < 0: data[2] = 0x10    ##If we are using negative values
-    
+    if joy_res == 12:
+        data[0] = 0x03
+        data[2] = 0x03
+
+    elif joy_res == 10:   
+        data[0] = 0x03
+        data[2] = 0x00
+    else:
+        data[0] = 0x00
+        data[2] = 0x00
+
+    if x < 0: data[0] = data[0] | 0x10
+    if y < 0: data[2] = data[2] | 0x10
+
     tmp = int(abs(x)).to_bytes(2,'big', signed=False)
-    data[0] = data[0] | (tmp[0] & 0x0c)
+    data[0] = data[0] | ((tmp[0] << 6) & 0xC0)  # 10 bit ...
+    data[0] = data[0] | (tmp[0] & 0x0c)         # 12 bit ...
+    data[1] = tmp[1]                            #  8 bit...
+
     tmp = int(abs(y)).to_bytes(2,'big', signed=False)
+    data[2] = data[2] | ((tmp[0] << 6) & 0xC0)
     data[2] = data[2] | (tmp[0] & 0x0c)
-    
+    data[3] = tmp[1]
+    #print(bytes(data))  ##Debugging print
+    #print(x,y)
     message = Message(id=id, data=bytes(data), extended=True)
-    print(bytes(data))  ##Debugging print    
     can_bus.send(message)
     await asyncio.sleep(0.1)    ##10hz
 
-#Read the joystick values, making using the midle of 10 readings.
+##Read the joystick values, making using the midle of 10 readings.
 async def read_joystick_position():
     while True:
         x_list = []
@@ -78,7 +96,7 @@ async def read_joystick_position():
         y = y_list[5]
         await send_joystick_position(x, y)
 
-#Listening on bus for filtered messages.
+##Listening on bus for filtered messages.
 async def listen_can(listener):
     while True:
         message_count = listener.in_waiting()
@@ -105,5 +123,3 @@ try:
     asyncio.run(main())
 except KeyboardInterrupt:
     print("Program ended")
-
-
